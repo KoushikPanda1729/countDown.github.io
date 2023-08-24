ed9dc4b65af303cdc7ccce78eef3ec59100d5a05
KOUSHIK <136024701+KoushikPanda1729@users.noreply.github.com>
2023-08-24 22:09:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const countDownClock = (endDate) => {
     const end = new Date(endDate);
     const now = new Date();
     const difference = (end - now) / 1000;
-
+     if (difference < 0) return;
     
     const days = Math.floor((difference / 3600) / 24);
     const hour = Math.floor(((difference / 3600) % 24));
     const minutes = Math.floor(((difference % 3600) / 60));
     const seconds = Math.floor(((difference % 3600) % 60));
-    if (seconds < 0) return;
+   
     inputAssign[0].value = days;
     inputAssign[1].value = hour;
     inputAssign[2].value = minutes;
